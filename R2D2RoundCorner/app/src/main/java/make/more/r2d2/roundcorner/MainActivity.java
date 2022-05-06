bb12a5c1fbd1b5f5dea0ed159ad356cb1812fa4e
lijunjun <gionchat@gmail.com>
2022-05-06 18:36:23 +0800

    init 4 project RoundCorner View ViewGroup

--- R2D2RoundCorner/app/src/main/java/make/more/r2d2/roundcorner/MainActivity.java
+++ R2D2RoundCorner/app/src/main/java/make/more/r2d2/roundcorner/MainActivity.java
@@ -1,6 +1,6 @@
 package make.more.r2d2.roundcorner;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
